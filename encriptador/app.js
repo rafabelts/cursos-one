ba642ccb1629e25d2145b29c65d2890c1ccc22e1
Rafael Beltran <rafabeltrans17@gmail.com>
2024-07-10 00:54:55 -0600

    Finished project

--- encriptador/app.js
+++ encriptador/app.js
@@ -6,9 +6,24 @@ const encriptationValues = {
   u: "ufat",
 };
 const desencriptationValues = {};
+
 for (let key in encriptationValues) {
   desencriptationValues[encriptationValues[key]] = key;
 }
+
+const showResult = (message) => {
+  const imgElement = document.querySelector("#result-container img");
+  const titleElement = document.querySelector("#result-container h1");
+  const paragraphElement = document.querySelector("#result-container p");
+  const containerElement = document.querySelector("#result-container");
+
+  imgElement.style.display = "none";
+  titleElement.style.display = "none";
+
+  paragraphElement.innerHTML = message;
+
+  containerElement.classList.add("show-result");
+};
 
 const getInput = () => {
   return document.getElementById("input").value;
@@ -17,12 +32,33 @@ const getInput = () => {
 const encript = () => {
   const inputValue = getInput();
 
-  console.log(
-    inputValue
-      .split("")
-      .map((letter) => encriptationValues[letter] || letter)
-      .join("")
-  );
+  const encryptedMessage = inputValue
+    .split("")
+    .map((letter) => encriptationValues[letter] || letter)
+    .join("");
+
+  showResult(encryptedMessage);
 };
 
-const desencript = () => {};
+const desencript = () => {
+  let inputValue = getInput();
+  let desencryptatedMessage = "";
+  let i = 0;
+
+  while (i < inputValue.length) {
+    let found = false;
+    for (let key in desencriptationValues) {
+      if (inputValue.substring(i, i + key.length) === key) {
+        desencryptatedMessage += desencriptationValues[key];
+        i += key.length;
+        found = true;
+        break;
+      }
+    }
+    if (!found) {
+      desencryptatedMessage += inputValue[i];
+      i++;
+    }
+  }
+  showResult(desencryptatedMessage);
+};
